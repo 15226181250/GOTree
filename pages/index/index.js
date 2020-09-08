f32429a0bccb9275ef9d274ca3bd91b3d3a9c952
unknown <unknown@example.com>
2020-09-08 15:28:01 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,165 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiper_list:[{
+      id:0,
+      src:"https://s1.ax1x.com/2020/09/08/wQ9KSS.jpg",
+    },{
+      id:1,
+      src:"https://s1.ax1x.com/2020/09/08/wQ9QyQ.jpg"
+    },{
+      id:2,
+      src:"https://s1.ax1x.com/2020/09/08/wQ9jXQ.jpg"
+    },{
+      id:3,
+      src:"https://s1.ax1x.com/2020/09/08/wQPpKe.jpg"
+    },{
+      id:4,
+      src:"https://s1.ax1x.com/2020/09/08/wQ9YF0.jpg"
+    },{
+      id:5,
+      src:"https://s1.ax1x.com/2020/09/08/wQPnKg.jpg"
+    },{
+      id:6,
+      src:"https://s1.ax1x.com/2020/09/08/wQPGGV.jpg"
+    }],
+    navigator_list:[{
+      id:0,
+      src:"https://s1.ax1x.com/2020/09/07/wnb380.png",
+      url:"/pages/seckill/index"
+    },{
+      id:1,
+      src:"https://s1.ax1x.com/2020/09/07/wnbGvT.png",
+      url:"/pages/empty/index"
+    },{
+      id:2,
+      src:"https://s1.ax1x.com/2020/09/07/wnbaVJ.png",
+      url:"/pages/empty/index"
+    },{
+      id:3,
+      src:"https://s1.ax1x.com/2020/09/07/wnbda9.png",
+      url:"/pages/empty/index"
+    }],
+    title_list:[{
+      id:0,
+      src:"https://s1.ax1x.com/2020/09/07/wuTVyD.png",
+    }],
+    list_one:[{
+      id:0,
+      src:"https://s1.ax1x.com/2020/09/07/wKAUYt.jpg",
+      name:"英语四级全真预测卷CET4",
+      cat_id:901,
+      prince:20.5,
+      author:"张三",
+      version:"2019-08",
+      original:68.5,
+      publish:"光明出版社",
+      type:"四级必备"
+    },{
+      id:1,
+      src:"https://s1.ax1x.com/2020/09/07/wKEI8P.jpg",
+      name:"概率论与数理统计",
+      cat_id:901,
+      prince:20.5,
+      author:"张三",
+      version:"2019-08",
+      original:68.5,
+      publish:"光明出版社",
+      type:"二手书"
+    },{
+      id:2,
+      src:"https://s1.ax1x.com/2020/09/07/wKVKKO.jpg",
+      name:"高等数学(上册)",
+      cat_id:901,
+      prince:20.5,
+      author:"张三",
+      version:"第七版",
+      original:68.5,
+      publish:"光明出版社",
+      type:"二手书"
+    },{
+      id:3,
+      src:"https://s1.ax1x.com/2020/09/07/wKE2HH.jpg",
+      name:"离散数学",
+      cat_id:901,
+      prince:20.5,
+      author:"张三",
+      version:"2019-08",
+      original:68.5,
+      publish:"光明出版社",
+      type:"二手书"
+    },{
+      id:4,
+      src:"https://s1.ax1x.com/2020/09/07/wKED9x.jpg",
+      name:"计算机组成原理",
+      cat_id:901,
+      prince:20.5,
+      author:"张三",
+      version:"2019-08",
+      original:68.5,
+      publish:"光明出版社",
+      type:"二手书"
+    }]
+
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
---
+++ pages/seckill/index.js
@@ -0,0 +1,71 @@
+// pages/seckill/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+    {id:0,src:"https://s1.ax1x.com/2020/09/07/wuFrRO.jpg",name:"oracle数据库管理开发与实践",price_o:52,price:2.5},
+    {id:1,src:"https://s1.ax1x.com/2020/09/07/wuamZ9.jpg",name:"python基础教程",price_o:79,price:3.4},
+    {id:2,src:"https://s1.ax1x.com/2020/09/07/wuadit.jpg",name:"java编程思想",price_o:108,price:4.2},
+    {id:3,src:"https://s1.ax1x.com/2020/09/07/wuafJ0.jpg",name:"nosql数据库原理",price_o:49.8,price:2.4},
+    {id:4,src:"https://s1.ax1x.com/2020/09/07/wuaOF1.jpg",name:"算法设计与分析",price_o:33,price:1.7}]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
